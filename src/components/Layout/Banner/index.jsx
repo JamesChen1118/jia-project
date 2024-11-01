4285211b462c5935281414558dbe7dabcaecc887
JamesChen <jameschencoding@gmail.com>
2024-11-01 15:31:48 +0800

    feat: Banner css switch Tailwind && delete Banner/index.css

--- src/components/Layout/Banner/index.jsx
+++ src/components/Layout/Banner/index.jsx
@@ -1,4 +1,3 @@
-import "./index.css";
 import { useTranslation } from "react-i18next";
 import { useUserStore } from "@/store/lang.js";
 
@@ -6,16 +5,22 @@ const Banner = () => {
   const { language } = useUserStore();
   const { t } = useTranslation();
   return (
-    <div className="banner">
-      <div className="banner-title">
-        <h3>JIA</h3>
-        <p>
+    <div className="relative">
+      <div className="absolute top-[350px] left-1/2 -translate-x-1/2 z-2">
+        <h3 className="relative text-main-color-yellow font-georgia text-8xl font-bold italic text-center tracking-[2.5rem]">
+          JIA
+        </h3>
+        <p className="text-[rgb(237,105,52)] text-4xl italic text-center mt-20 tracking-[5px] word-spacing-[1.8rem]">
           {language === "zh_TW" ? "現代 " : t("Modern ")}
           {language === "zh_TW" ? "創作 " : t("Creative ")}
           {language === "zh_TW" ? "居酒屋" : t("Izakaya")}
         </p>
       </div>
-      <img src="/src/assets/images/home/banner-1.png" alt="" />
+      <img
+        src="/src/assets/images/home/banner-1.png"
+        alt=""
+        className="w-full h-[80vh] opacity-40 object-cover"
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ // 新增
+  content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'georgia': ['Georgia', 'Times New Roman', 'Times', 'serif'],
+      },
       colors: {
         'main-color-yellow': 'rgb(255, 170, 13)',
       },
