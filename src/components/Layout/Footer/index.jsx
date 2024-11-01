4d0d31a1287832c0d4290122049f35516046bab1
JamesChen <jameschencoding@gmail.com>
2024-11-01 15:23:41 +0800

    feat: Footer css switch Tailwind && delete Footer/index.css

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
@@ -9,17 +8,28 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <h2 className="footer-logo">J I A</h2>
+    <footer className="py-5 h-[150px] text-lg text-main-color-yellow bg-[rgb(26,26,26)] flex justify-around items-center">
+      <h2 className="font-verdana font-bold text-4xl text-main-color-yellow">
+        J I A
+      </h2>
       <p>&copy; JIA 2024 - Modern Creative Izakaya</p>
-      <div className="social-icons">
-        <Link to="#" className="icon-btn">
+      <div className="flex justify-center mt-5">
+        <Link
+          to="#"
+          className="text-white text-2xl mx-2.5 transition-all duration-300 hover:text-main-color-yellow hover:scale-110 cursor-pointer"
+        >
           <FontAwesomeIcon icon={faFacebookF} />
         </Link>
-        <Link to="#" className="icon-btn">
+        <Link
+          to="#"
+          className="text-white text-2xl mx-2.5 transition-all duration-300 hover:text-main-color-yellow hover:scale-110 cursor-pointer"
+        >
           <FontAwesomeIcon icon={faInstagram} />
         </Link>
-        <Link to="#" className="icon-btn">
+        <Link
+          to="#"
+          className="text-white text-2xl mx-2.5 transition-all duration-300 hover:text-main-color-yellow hover:scale-110 cursor-pointer"
+        >
           <FontAwesomeIcon icon={faTwitter} />
         </Link>
       </div>
