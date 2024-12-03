76382ea880df600057db8cae9e105e5c6c01a780
JamesChen <jameschencoding@gmail.com>
2024-12-03 14:59:32 +0800

    feat: change .env added VITE_API_URL

--- src/api/module/news.js
+++ src/api/module/news.js
@@ -4,7 +4,6 @@ export const newsApi = {
     getNews: async () => {
         try {
             const { data } = await server.get("/news");
-            console.log("Fetched news data:", data);
             return data;
         } catch (error) {
             console.error("Error in getNews:", error);
